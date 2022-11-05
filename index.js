524c9a56fe3ce990241c11bd73d8ef8bf5d8ae97
vutatphat <casngu@gmail.com>
2022-11-05 06:25:35 -0500

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('hello')
+let idDiv = document.getElementById('div1')
+console.log(idDiv)
+console.dir(idDiv)
+idDiv.textContent = ('ông tuân ngu')
+
+idDiv.addEventListener('click',()=>{console.log("clicked")
+
+})
+
